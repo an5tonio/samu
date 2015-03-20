8a60b639648533adb2d1247d05470457ad2c635e
Antonio Junior <an5tonio@gmail.com>
2015-03-20 03:47:38 -0300

    tentando calcular a menor distancia

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
 
-class vertex
+class vertice
 {
-    public $key         = null;
-    public $visited     = 0;
-    public $distance    = 1000000;  // infinite
-    public $parent      = null;
-    public $path        = null;
+    public $chave         = null;
+    public $visitado     = 0;
+    public $distancia    = 1000000;  // infinite
+    public $parente      = null;
+    public $caminho        = null;
  
-    public function __construct($key) 
+    public function __construct($chave) 
     {
-        $this->key  = $key;
+        $this->chave  = $chave;
     }
 }
  
@@ -23,44 +23,44 @@ class PriorityQueue extends SplPriorityQueue
     }
 }
  
-$v0 = new vertex(0);
-$v1 = new vertex(1);
-$v2 = new vertex(2);
-$v3 = new vertex(3);
-$v4 = new vertex(4);
-$v5 = new vertex(5);
+$v0 = new vertice(0);
+$v1 = new vertice(1);
+$v2 = new vertice(2);
+$v3 = new vertice(3);
+$v4 = new vertice(4);
+$v5 = new vertice(5);
  
 $list0 = new SplDoublyLinkedList();
-$list0->push(array('vertex' => $v1, 'distance' => 3));
-$list0->push(array('vertex' => $v3, 'distance' => 1));
+$list0->push(array('vertice' => $v1, 'distancia' => 3));
+$list0->push(array('vertice' => $v3, 'distancia' => 1));
 $list0->rewind();
  
 $list1 = new SplDoublyLinkedList();
-$list1->push(array('vertex' => $v0, 'distance' => 3));
-$list1->push(array('vertex' => $v2, 'distance' => 7));
+$list1->push(array('vertice' => $v0, 'distancia' => 3));
+$list1->push(array('vertice' => $v2, 'distancia' => 7));
 $list1->rewind();
  
 $list2 = new SplDoublyLinkedList();
-$list2->push(array('vertex' => $v1, 'distance' => 7));
-$list2->push(array('vertex' => $v3, 'distance' => 8));
-$list2->push(array('vertex' => $v4, 'distance' => 12));
+$list2->push(array('vertice' => $v1, 'distancia' => 7));
+$list2->push(array('vertice' => $v3, 'distancia' => 8));
+$list2->push(array('vertice' => $v4, 'distancia' => 12));
 $list2->rewind();
  
 $list3 = new SplDoublyLinkedList();
-$list3->push(array('vertex' => $v0, 'distance' => 1));
-$list3->push(array('vertex' => $v2, 'distance' => 8));
+$list3->push(array('vertice' => $v0, 'distancia' => 1));
+$list3->push(array('vertice' => $v2, 'distancia' => 8));
 $list3->rewind();
  
 $list4 = new SplDoublyLinkedList();
-$list4->push(array('vertex' => $v2, 'distance' => 12));
-$list4->push(array('vertex' => $v5, 'distance' => 3));
+$list4->push(array('vertice' => $v2, 'distancia' => 12));
+$list4->push(array('vertice' => $v5, 'distancia' => 3));
 $list4->rewind();
  
 $list5 = new SplDoublyLinkedList();
-$list5->push(array('vertex' => $v4, 'distance' => 3));
+$list5->push(array('vertice' => $v4, 'distancia' => 3));
 $list5->rewind();
  
-$adjacencyList = array(
+$listaDeAdjacencias = array(
     $list0,
     $list1,
     $list2,
@@ -68,53 +68,76 @@ $adjacencyList = array(
     $list4,
     $list5,
 );
+
+
  
-function calcShortestPaths(vertex $start, &$adjLists)
+function calculoMenorCaminho(vertice $inicio, &$listaDeAdjacencias)
 {
+    foreach ($listaDeAdjacencias as $value) {
+        $teste[] = new $value;
+    }
+    
+    $somaDistancia = 0;
     // define an empty queue
     $q = new PriorityQueue();
  
-    // push the starting vertex into the queue
-    $q->insert($start, 0);
+    // push the starting vertice into the queue
+    $q->insert($inicio, 0);
     $q->rewind();
  
-    // mark the distance to it 0
-    $start->distance = 0;
+    // mark the distancia to it 0
+    $inicio->distancia = 0;
  
-    // the path to the starting vertex
-    $start->path = array($start->key);
+    // the caminho to the starting vertice
+    $inicio->caminho = array($inicio->chave);
  
     while ($q->valid()) {
         $t = $q->extract();
-        $t->visited = 1;
+        $t->visitado = 1;
  
-        $l = $adjLists[$t->key];
+        $l = $listaDeAdjacencias[$t->chave];
         while ($l->valid()) {
             $item = $l->current();
  
-            if (!$item['vertex']->visited) {
-                if ($item['vertex']->distance > $t->distance + $item['distance']) {
-                    $item['vertex']->distance = $t->distance + $item['distance'];
-                    $item['vertex']->parent = $t;
+            if (!$item['vertice']->visitado) {
+                if ($item['vertice']->distancia > $t->distancia + $item['distancia']) 
+                {
+                    $item['vertice']->distancia = $t->distancia + $item['distancia'];
+                    $item['vertice']->parente = $t;
+                    $somaDistancia = $somaDistancia + $t->distancia ;
                 }
  
-                $item['vertex']->path = array_merge($t->path, array($item['vertex']->key));
- 
-                $q->insert($item["vertex"], $item["vertex"]->distance);
+                $item['vertice']->caminho = array_merge($t->caminho, array($item['vertice']->chave));
+                $q->insert($item["vertice"], $item["vertice"]->distancia);
             }
             $l->next();
         }
+        
         $q->recoverFromCorruption();
         $q->rewind();
     }
+    
+    echo 'A distancia do vertice  '. $inicio->chave. ' para os demais e ' .$somaDistancia;
+    
+    print_r($teste);
 }
  
-calcShortestPaths($v0, $adjacencyList);
- 
-// The path from node 0 to node 5
-// [0, 1, 2, 4, 5]
-echo '[' . implode(', ', $v5->path) . ']'.PHP_EOL;
-echo  $v5->distance.PHP_EOL;
+calculoMenorCaminho($v2, $listaDeAdjacencias);
 
-echo '[' . implode(', ', $v4->path) . ']'.PHP_EOL;
-echo  $v4->distance;
+echo '[' . implode(', ', $v5->caminho) . ']'.PHP_EOL;
+echo  $v5->distancia.PHP_EOL;
+
+echo '[' . implode(', ', $v4->caminho) . ']'.PHP_EOL;
+echo  $v4->distancia;
+
+echo '[' . implode(', ', $v3->caminho) . ']'.PHP_EOL;
+echo  $v3->distancia;
+
+echo '[' . implode(', ', $v2->caminho) . ']'.PHP_EOL;
+echo  $v2->distancia;
+
+echo '[' . implode(', ', $v1->caminho) . ']'.PHP_EOL;
+echo  $v1->distancia;
+
+echo '[' . implode(', ', $v0->caminho) . ']'.PHP_EOL;
+echo  $v0->distancia;
